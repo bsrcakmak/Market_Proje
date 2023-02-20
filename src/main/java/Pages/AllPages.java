3a5f02ce3dae8241a510c9f5c915d1f80d6a6cc0
bsrcakmak <bsrckmk516@gmail.com>
2023-02-20 23:56:22 +0300

    market_task

--- src/main/java/Pages/AllPages.java
+++ src/main/java/Pages/AllPages.java
@@ -1,8 +1,15 @@
 package Pages;
+
 public class AllPages {
     public AllPages(){}
+
     private MainPage mainPage;
     private LoginPage loginPage;
+    private UserDashboardPage userDashboardPage;
+    private SearchResultPage searchResultPage;
+    private ProductDetailPage productDetailPage;
+    private CartPage cartPage;
+
     public MainPage mainPage(){
         if (mainPage==null){
             mainPage=new MainPage();
@@ -14,5 +21,29 @@ public class AllPages {
             loginPage=new LoginPage();
         }
         return loginPage;
+    }
+    public UserDashboardPage userDashboardPage() {
+        if (userDashboardPage == null) {
+            userDashboardPage = new UserDashboardPage();
+        }
+        return userDashboardPage;
+    }
+    public SearchResultPage searchResultPage(){
+        if (searchResultPage==null){
+            searchResultPage=new SearchResultPage();
+        }
+        return searchResultPage;
+    }
+    public ProductDetailPage productDetailPage(){
+        if (productDetailPage==null){
+            productDetailPage=new ProductDetailPage();
+        }
+        return productDetailPage;
+    }
+    public CartPage cartPage(){
+        if (cartPage==null){
+            cartPage=new CartPage();
+        }
+        return cartPage;
     }
 }
--- src/test/java/TC001.java
+++ src/test/java/TC001.java
@@ -1,20 +1,40 @@
 import Base.BaseTest;
+import Listener.Listener;
 import Pages.AllPages;
 import Utilities.ConfigReader;
+import com.opencsv.exceptions.CsvException;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import static ExtentReports.ExtentTestManager.startTest;
+
+@Listeners({Listener.class})
+
 public class TC001 extends BaseTest {
-    AllPages allPages = new AllPages();
+    AllPages allPages=new AllPages();
+    @Test(description = "Kullanıcı girişi yapılarak sepete ürün eklenmesi")
+    public void test01(Method method) throws IOException, CsvException {
 
-    @Test
+        startTest(method.getName(),"Kullanıcı girişi yapılarak sepete ürün eklenmesi");
+        allPages.mainPage().go_To_Url(ConfigReader.getProperty("baseURL"));
+        allPages.mainPage().hoverOverGirisYapButon();
+        allPages.loginPage().loginHepsiBurada(ConfigReader.getProperty("UserMail"),ConfigReader.getProperty("UserPassword"));
+        allPages.userDashboardPage().verifyValidLogin("test");
+        allPages.userDashboardPage().searchProduct("robot süpürge");
+        allPages.searchResultPage().SearchResultSummary("robot süpürge");
+        allPages.searchResultPage().selectProduct();
+        allPages.productDetailPage().add_To_Cart();
+        allPages.cartPage().go_To_Cart();
+        allPages.cartPage().verify_To_ProductsOnCart();
 
-    public void test01() {
-        allPages.mainPage().go_To_Url(ConfigReader.getProperty("baseUrl"));
-        allPages.mainPage().hoverOverGirisYapButton();
+
     }
+
+
 }
-
-
 
 //1. Kullanıcı girişi yapılarak sepete ürün eklenmesi
 // Kullanıcı Hepsiburada.com sitesini ziyaret eder.
